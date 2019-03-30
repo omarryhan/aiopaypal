92ed74e0e9fcd40fcbfa897e4b6068efe2dc900c
Omar Ryhan <omarryhan@gmail.com>
2019-03-30 10:20:13 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+import os
+from os import listdir
+from os.path import isfile, join
+mypath = os.path.dirname(os.path.abspath(__file__))
+print([f for f in listdir(mypath) if isfile(join(mypath, f))])
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name='aiopaypal',
+    version="0.1.0",
+    author='Omar Ryhan',
+    author_email='omarryhan@gmail.com',
+    license='GNU',
+    description="Async Paypal client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'aiohttp',
+        'aiofiles',
+        'pyopenssl'
+    ],
+    tests_require=[
+        'aiohttp',
+        'aiofiles',
+        'pyopenssl',
+        'pytest',
+        'pytest-asyncio'
+    ],
+    tests_require=test_requirements,
+    url='https://github.com/omarryhan/aiopaypal',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+)
